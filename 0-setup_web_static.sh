6a6773fca6a63eede9144b6bdeea9e63b3c6141f
Kefikhalil <naroqt@gmail.com>
2020-08-17 14:33:23 -0700

    updated

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#setup_web_static
+apt -y update
+apt install -y nginx
+mkdir -pv /data/web_static/releases/test/
+mkdir -pv /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+echo "
+server {
+  listen 80;
+  listen [::]:80 default_server;
+  location /hbnb_static {
+    alias /data/web_static/current/;
+  }
+  index index.html;
+  server_name nabq.tech;
+  rewrite '^/redirect_me$' http://example.com permanent;
+  error_page 404 /custom_404.html;
+  add_header X-Served-By $HOSTNAME;
+}" > /etc/nginx/sites-available/default
+service nginx restart
